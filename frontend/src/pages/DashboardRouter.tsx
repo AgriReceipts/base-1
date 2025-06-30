da44e010f74bb8a7ae4320def1bd19e4176d9ba4
Sasi Kumar <sasidev272004@gmail.com>
2025-06-30 16:00:28 +0530

    fixed deo dashboard

--- frontend/src/pages/DashboardRouter.tsx
+++ frontend/src/pages/DashboardRouter.tsx
@@ -1,35 +1,28 @@
-import React from 'react';
-import {Navigate} from 'react-router-dom';
-import DeoDashboard from './Deo/Dashboard';
-import SupervisorDashboard from './Supervisor/Dashboard';
-import AdDashboard from './Ad/Dashboard';
-import {useAuthStore} from '../stores/authStore';
-import NavBar from '@/components/ui/NavBar';
+import React from "react";
+import { Navigate } from "react-router-dom";
+import DeoDashboard from "./Deo/Dashboard";
+import SupervisorDashboard from "./Supervisor/Dashboard";
+import AdDashboard from "./Ad/Dashboard";
+import { useAuthStore } from "../stores/authStore";
 
 const DashboardRouter: React.FC = () => {
-  const {role} = useAuthStore();
+  const { role } = useAuthStore();
 
   if (!role) {
-    return <Navigate to='/login' replace={true} />;
+    return <Navigate to="/login" replace={true} />;
   }
-
-  let DashboardComponent: React.ReactNode;
 
   switch (role) {
-    case 'deo':
-      DashboardComponent = <DeoDashboard />;
-      break;
-    case 'supervisor':
-      DashboardComponent = <SupervisorDashboard />;
-      break;
-    case 'ad':
-      DashboardComponent = <AdDashboard />;
-      break;
+    case "deo":
+      return <DeoDashboard />;
+    case "supervisor":
+      return <SupervisorDashboard />;
+    case "ad":
+      return <AdDashboard />;
     default:
-      return <Navigate to='/login' replace={true} />;
+      return <Navigate to="/login" replace={true} />;
   }
-
-  return (
+   return (
     <div className='max-w-screen-2xl mx-auto bg-gray-50'>
       <NavBar />
       {DashboardComponent}
@@ -38,3 +31,4 @@ const DashboardRouter: React.FC = () => {
 };
 
 export default DashboardRouter;
+
