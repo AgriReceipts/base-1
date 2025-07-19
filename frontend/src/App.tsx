9c929ee21200863124eb5a32bb5fe1ee60401ff5
Srivathsa <121475793+Srivathsa252@users.noreply.github.com>
2025-07-20 02:36:21 +0530

    Setup to run landing page only

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,9 @@
 import React, {useEffect} from 'react';
-import {BrowserRouter} from 'react-router-dom';
-import {AppRouter} from './routes/AppRouter';
-import {AuthProvider} from './providers/authProvider';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-    </AuthProvider>
+    <LandingPage />
   );
 }
 
